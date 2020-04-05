c238d8e02f5f3f88f95c798d2496383910c690c4
Chintan Palan <chintan.palan@techno-soft.in>
2020-04-05 14:30:56 +0530

    Initial Idea

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Col, Row, Slider} from 'antd';
+import 'antd/dist/antd.min.css';
+import "./App.css"
+
+const initialWidth = 240;
+const initialFontSize = 16;
+const widthInEm = initialWidth / initialFontSize;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [fontSize, setFontSize] = useState(initialFontSize);
+    // const [ bodyFontSize, setBodyFontSizeActual ] = useState(initialFontSize);
+    // const setBodyFontSize = fs => {
+    //     window.document.body.style.fontSize = fs + "px";
+    //     setBodyFontSizeActual(fs);
+    // };
+    return (
+        <div>
+            <Row>
+                <Col span={8} offset={8}>
+                    <h2>Font Size</h2>
+                    <Slider value={fontSize} min={5} max={40} onChange={setFontSize}/>
+                </Col>
+            </Row>
+            <div className="demo" style={{fontSize}}>
+                <div>
+                    <p>Actual Width: {initialWidth}px</p>
+                    <p>Padding: {initialFontSize}px</p>
+                    <div className={"window"}>
+                        <div
+                            className={"content px"}
+                            style={{
+                                width: initialWidth + "px",
+                                padding: initialFontSize + "px",
+                            }}
+                        >
+                            <h2>PX Sized</h2>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p>Actual Width: {fontSize * widthInEm}px</p>
+                    <p>Padding: 1em ({fontSize}px)</p>
+                    <div className={"window"}>
+                        <div
+                            className={"content em"}
+                            style={{
+                                width: widthInEm + "em",
+                                padding: "1em",
+                            }}
+                        >
+                            <h2>EM Sized</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
